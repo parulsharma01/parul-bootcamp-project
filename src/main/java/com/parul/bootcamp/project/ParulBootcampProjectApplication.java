351a885b34fd228ec2362540d9bbda3167084c5d
Parul Sharma <parul.sharma1@tothenew.com>
2022-03-23 15:54:53 +0530

    Init commit

---
+++ src/main/java/com/parul/bootcamp/project/ParulBootcampProjectApplication.java
@@ -0,0 +1,14 @@
+package com.parul.bootcamp.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParulBootcampProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParulBootcampProjectApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/parul/bootcamp/project/ParulBootcampProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parul.bootcamp.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParulBootcampProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
