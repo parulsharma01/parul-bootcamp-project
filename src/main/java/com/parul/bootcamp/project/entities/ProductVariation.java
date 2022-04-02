f5002456b1e22e2addb7ec883fc22e683aacefba
Parul Sharma <parul.sharma1@tothenew.com>
2022-04-02 16:35:04 +0530

    Order and Cart entities added

--- src/main/java/com/parul/bootcamp/project/entities/ProductVariation.java
+++ src/main/java/com/parul/bootcamp/project/entities/ProductVariation.java
@@ -12,4 +12,60 @@ public class ProductVariation {
     private boolean isActive;
     private Product productId;
     private String metadata;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Product getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Product productId) {
+        this.productId = productId;
+    }
+
+    public String getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(String metadata) {
+        this.metadata = metadata;
+    }
 }
